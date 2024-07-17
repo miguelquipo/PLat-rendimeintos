d611101f458757558e4ded3a31b894c7cc58e327
Miguel <quipomiguel42@gmail.com>
2024-07-17 10:20:34 -0500

    V 1.1.61 Ingreso y actualizacion de productos

--- PHP/ingresoProd.php
+++ PHP/ingresoProd.php
@@ -4,35 +4,10 @@ include 'db.php';
 if (isset($_POST['product-name'], $_POST['product-rendimiento-hora'])) {
     $productName = $_POST['product-name'];
     $productRendimientoHora = $_POST['product-rendimiento-hora'];
+    $productId = $_POST['product-id'];
 
-    // Verificar si el nombre del producto ya existe en la base de datos
-    $sql_check = "SELECT * FROM productos WHERE nombre_producto = ?";
-    $params = array($productName);
-    $stmt = sqlsrv_query($conn, $sql_check, $params);
-
-    if ($stmt === false) {
-        echo 'Hubo un error al realizar la consulta';
-        exit();
-    }
-
-    $row_count = sqlsrv_has_rows($stmt);
-
-    if ($row_count === true) {
-        // Si el nombre del producto ya existe, realizar una actualización
-        $sql_update = "UPDATE productos SET rendimiento_producto_hora = ? WHERE nombre_producto = ?";
-        $params_update = array($productRendimientoHora, $productName);
-        $stmt_update = sqlsrv_query($conn, $sql_update, $params_update);
-
-        if ($stmt_update === false) {
-            echo 'Hubo un error al actualizar el Producto';
-            exit();
-        } else {
-            echo 'Se ha actualizado correctamente';
-            header("Location: ../HTML/ingProductos1.html"); // Redirigir con mensaje de éxito
-            exit();
-        }
-    } else {
-        // Si el nombre del producto no existe, realizar la inserción
+    if (empty($productId)) {
+        // Insertar un nuevo producto
         $sql_insert = "INSERT INTO productos (nombre_producto, rendimiento_producto_hora) VALUES (?, ?)";
         $params_insert = array($productName, $productRendimientoHora);
         $stmt_insert = sqlsrv_query($conn, $sql_insert, $params_insert);
@@ -42,6 +17,20 @@ if (isset($_POST['product-name'], $_POST['product-rendimiento-hora'])) {
             exit();
         } else {
             echo 'Se ha ingresado correctamente';
+            header("Location: ../HTML/ingProductos1.html"); // Redirigir con mensaje de éxito
+            exit();
+        }
+    } else {
+        // Actualizar producto existente
+        $sql_update = "UPDATE productos SET nombre_producto = ?, rendimiento_producto_hora = ? WHERE id_producto = ?";
+        $params_update = array($productName, $productRendimientoHora, $productId);
+        $stmt_update = sqlsrv_query($conn, $sql_update, $params_update);
+
+        if ($stmt_update === false) {
+            echo 'Hubo un error al actualizar el Producto';
+            exit();
+        } else {
+            echo 'Se ha actualizado correctamente';
             header("Location: ../HTML/ingProductos1.html"); // Redirigir con mensaje de éxito
             exit();
         }
